95660d8336b8215e234422fcb718b062ebde7cf5
ssztnt <146991272+ssztnt@users.noreply.github.com>
2024-10-14 16:50:35 +0300

    lab01

---
+++ year2/semestrul1/MAP/Laboratoare/Model/OperatieInmultire.java
@@ -0,0 +1,13 @@
+package Model;
+
+import static Enum.Operatie.MULTIPLY;
+
+public class OperatieInmultire extends ComplexExpression {
+    public OperatieInmultire(NumarComplex[] args) {
+        super(MULTIPLY, args);
+    }
+
+    public NumarComplex executeOneOperation(NumarComplex nr1, NumarComplex nr2) {
+        return nr1.inmultire(nr2);
+    }
+}
