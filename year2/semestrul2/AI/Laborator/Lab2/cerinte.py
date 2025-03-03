f6282683ac994903f8eb56d690243c0d6b4fb52d
ssztnt <rares.stefan@stud.ubbcluj.ro>
2025-03-03 17:23:37 +0200

    o schimbare

--- year2/semestrul2/AI/Laborator/Lab2/cerinte.py
+++ year2/semestrul2/AI/Laborator/Lab2/cerinte.py
@@ -3,11 +3,41 @@ import csv
 # Citirea fișierului CSV
 file_path = '/Users/plaiurares/cs-ubb/year2/semestrul2/AI/Laborator/Lab2/surveyDataSience.csv'
 
-with open(file_path, mode='r') as file:
-    reader = csv.reader(file)
-    # Sărim peste antet
-    next(reader)
-    # Numărăm rândurile
-    num_respondents = sum(1 for row in reader)
 
-print(f"Numărul de respondenți: {num_respondents}")
+def read_csv(file_path):
+    with open(file_path, mode='r') as file:
+        reader = csv.reader(file)
+        # Sărim peste antet
+        next(reader)
+        # Numărăm rândurile
+        num_respondents = sum(1 for row in reader)
+
+    print(f"Numărul de respondenți: {num_respondents}"  )
+
+def numar_si_tip_atribute(file_path):
+    with open(file_path, mode='r') as file:
+        reader = csv.reader(file)
+        # Citim antetul
+        header = next(reader)
+        num_atribute = len(header)
+        tip_atribute = [type(value).__name__ for value in header]
+        
+    print(f"Numărul de atribute: {num_atribute}")
+    print(f"Tipurile de atribute: {tip_atribute}")
+
+def numar_respondenti_completi(file_path):
+    with open(file_path, mode='r') as file:
+        reader = csv.reader(file)
+        # Citim antetul
+        header = next(reader)
+        num_atribute = len(header)
+        # Numărăm rândurile complete
+        num_respondenti_completi = sum(1 for row in reader if len(row) == num_atribute)
+
+def main():
+    read_csv(file_path)
+    numar_si_tip_atribute(file_path)
+    numar_respondenti_completi(file_path)
+
+main()
+    
