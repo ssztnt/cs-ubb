c00e47906e1da3e1da8e5ce8b750893d174c7795
Plaiu Rares-Ștefan <146991272+ssztnt@users.noreply.github.com>
2025-03-11 14:16:12 +0200

    Delete MPP directory

--- MPP/Laboratoare/demotest/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.demotest {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.demotest to javafx.fxml;
-    exports org.example.demotest;
-}
--- MPP/Laboratoare/lab3_java1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.lab3_java1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.lab3_java1 to javafx.fxml;
-    exports org.example.lab3_java1;
-}
