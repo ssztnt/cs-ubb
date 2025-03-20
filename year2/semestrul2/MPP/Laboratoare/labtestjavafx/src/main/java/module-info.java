9870be016ee3b06da16c2e7309f0132dbda90e9c
ssztnt <rares.stefan@stud.ubbcluj.ro>
2025-03-20 22:42:28 +0200

    deleted labs

--- year2/semestrul2/MPP/Laboratoare/labtestjavafx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.labtestjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.labtestjavafx to javafx.fxml;
-    exports org.example.labtestjavafx;
-}
